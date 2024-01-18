205245e368f033f0280c977075f32496a8937e94
barbodak <barbodak@gmail.com>
2024-01-18 13:10:29 +0330

    Added quiz info model

--- AccuracyTestBackend/quiz/models.py
+++ AccuracyTestBackend/quiz/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuizInfo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    start_time = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f'{self.user.username} : {self.start_time}'
--- AccuracyTestBackend/quiz/views.py
+++ AccuracyTestBackend/quiz/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from datetime import timedelta
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.utils import timezone
+from .models import QuizInfo
 
+
+def startQuiz(request):
+    print(timezone.now())
+    quiz = QuizInfo.objects.filter(user__username='barbodak').update(start_time=timezone.now() - timedelta(hours=15))
+    return HttpResponse(status=200)
 # Create your views here.
