6a83a4f973e39d5b2dc2432ac190a652f26500fd
Barbod Coliaie <98666418+barbodak@users.noreply.github.com>
2024-02-09 17:31:10 +0330

    Update settings.py

--- AccuracyTestBackend/AccuracyTestBackend/settings.py
+++ AccuracyTestBackend/AccuracyTestBackend/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
+    'django.contrib.staticfiles.middleware.StaticFileMiddleware',
 ]
 
 
