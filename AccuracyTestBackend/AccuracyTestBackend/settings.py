97dc2e11bd1539d954c2fa18c12b4d8978fad29e
Barbod Coliaie <98666418+barbodak@users.noreply.github.com>
2024-02-09 17:01:20 +0330

    Update settings.py

--- AccuracyTestBackend/AccuracyTestBackend/settings.py
+++ AccuracyTestBackend/AccuracyTestBackend/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
+import os 
+import sys
 import environ
 from pathlib import Path
 
@@ -168,8 +169,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/usr/src/app/static/'
-STATIC_ROOT = '/usr/src/app/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 
 # Default primary key field type
