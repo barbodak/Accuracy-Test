b2f62d0b7c8c1935cd2a165f93d4282db7105af3
barbodak <barbodak@gmail.com>
2024-02-11 22:21:47 +0330

    Added ValuTest res

--- AccuracyTestBackend/quiz/views.py
+++ AccuracyTestBackend/quiz/views.py
@@ -34,6 +34,23 @@ class QuizViewSet(viewsets.ViewSet):
                 raise ValueError(f"Unrecognized quiz type: {quiz_type}")
         return quiz
 
+    def calculateValuTestResult(self, quiz):
+        answers = quiz.answers
+        for i in range(20):
+            if answers[i] in (3, 7, 10, 14, 18, 19):
+                quiz.sharayet_kari += (i % 5) + 1
+            if answers[i] in (2, 16, 17):
+                quiz.hemayat += ((i % 5) + 1) * 2
+            if answers[i] in (8, 11, 15):
+                quiz.ravabet += ((i % 5) + 1) * 2
+            if answers[i] in (4, 5, 12):
+                quiz.pishraft += ((i % 5) + 1) * 2
+            if answers[i] in (9, 13, 20):
+                quiz.esteghlal += ((i % 5) + 1) * 2
+            if answers[i] in (1, 6):
+                quiz.tofigh += ((i % 5) + 1) * 3
+            quiz.save()
+
     def startQuiz(self, request, quiz_type):
         quiz = self.get_quiz(quiz_type, request.user)
         if quiz is None:
@@ -57,6 +74,9 @@ class QuizViewSet(viewsets.ViewSet):
             case "ValuTest":
                 if (timezone.now() - quiz.quiz_info.start_time).seconds > 60 * 180:
                     return HttpResponse(status=400)
+                else:
+                    self.calculateValuTestResult(quiz)
+
         print(request.data.get('answers'))
         quiz.answers = request.data.get('answers')
         quiz.save()
