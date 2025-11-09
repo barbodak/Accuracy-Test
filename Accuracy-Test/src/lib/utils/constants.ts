1c8bd42f931338a96351d86ed9010e8c8388f0b8
Barbod Coliaie <98666418+barbodak@users.noreply.github.com>
2025-11-09 14:39:49 +0330

    Update constants.ts

--- Accuracy-Test/src/lib/utils/constants.ts
+++ Accuracy-Test/src/lib/utils/constants.ts
@@ -1,3 +1,3 @@
-export const BASE_API_URL = "http://10.82.203.254:8000/api";
-export const BASE_internal_URL = "http://10.82.203.254:5173/ValuTest/result";
+export const BASE_API_URL = "https://api-quiz-project.darkube.app//api";
+export const BASE_internal_URL = "https://quizapp.darkube.app/ValuTest/result";
 export const COOKIE_MAX_AGE = 60 * 60 * 24 * 7; // 7 days
