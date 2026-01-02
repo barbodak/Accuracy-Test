3415c446e090d3fff43659fe958daa45952461e2
root <root@srv3783398445.bitcommand.net>
2026-01-02 22:56:26 +0000

    fixed Docker for front

--- Accuracy-Test/src/lib/utils/constants.ts
+++ Accuracy-Test/src/lib/utils/constants.ts
@@ -1,3 +1,3 @@
-export const BASE_API_URL = "https://api-quiz-project.darkube.app/api";
+export const BASE_API_URL = "http://45.149.77.96:8000/api";
 export const BASE_internal_URL = "https://quizapp.darkube.app/ValuTest/result";
 export const COOKIE_MAX_AGE = 60 * 60 * 24 * 7; // 7 days
